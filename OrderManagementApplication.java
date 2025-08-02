a55aac75de4115f45674d00bf58e2eea049b1344
Prabhat Jha <154579151+Prabhatjha06@users.noreply.github.com>
2025-08-03 00:35:13 +0530

    Create OrderManagementApplication.java

---
+++ OrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.orderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class OrderManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementApplication.class, args);
+    }
+}
